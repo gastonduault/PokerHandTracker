273046f20ed7d0052ca120f8605dfa8f6b163d14
gastonduault <gas.duault@gmail.com>
2021-09-12 21:41:10 +0200

    anime choix carte front end>> 3h

--- app.js
+++ app.js
@@ -40,23 +40,84 @@ window.addEventListener("resize", () => {
 
 
 /*--------------burger liste carte--*/
-const burger = document.querySelector(".zoneClickCarte1");
 const listCarte = document.querySelector(".listCarte1");
-
-burger.addEventListener('click', () => {
-    listCarte.classList.toggle("active");
-});
-
-//const cartes = document.querySelectorAll('.cartes2','.cartes3','.cartes4','.cartes5','.cartes6','.cartes7','.cartes8','.cartes9','.cartes10','.cartesV','.cartesQ','.cartesK','.cartesA');
 const cartes = listCarte.querySelectorAll(".cartes");
-let carteSelect = menu.querySelector(".select");
-function choixCarte(item){
-    if(carteSelect == item) return;
-    else if(carteSelect){
-        carteSelect.classList.remove("active")
-    }
-    item.classList.toggle("select");
+const cartesS = listCarte.querySelectorAll(".carteS");
+
+function cardClear(){
+    cartes.forEach((item)=>{
+        item.classList.remove("select");
+    });
 }
-cartes.forEach((item,index) => {
-    item.addEventListener('click', () => choixCarte(item));
+
+
+function choixCarte(index){
+    listCarte.style.height='auto';
+    let carteSelect = listCarte.querySelector(".select");
+    if(cartes[index] == carteSelect){
+        carteSelect.classList.remove("select");
+    }else{
+        cardClear();
+        cartes[index].classList.toggle("select");
+    }
+}
+cartesS.forEach((item, index) => {
+    item.addEventListener('click', () => choixCarte(index));
 })
+
+function heightClose(element) {
+  // get the height of the element's inner content, regardless of its actual size
+  var sectionHeight = element.scrollHeight;
+  // temporarily disable all css transitions
+  var elementTransition = element.style.transition;
+  element.style.transition = '';
+  // on the next frame (as soon as the previous style change has taken effect),
+  // explicitly set the element's height to its current pixel height, so we 
+  // aren't transitioning out of 'auto'
+  requestAnimationFrame(function() {
+    element.style.height = sectionHeight + 'px';
+    element.style.transition = elementTransition;
+    // on the next frame (as soon as the previous style change has taken effect),
+    // have the element transition to height: 0
+    requestAnimationFrame(function() {
+      element.style.height = 30 + 'px';
+    });
+  });
+}
+
+function heightOpen(element) {
+    /*var sectionHeight = element.scrollHeight;
+
+    var elementTransition = element.style.transition;
+    element.style.transition = '';
+    requestAnimationFrame(function() {
+        element.style.height = 'auto';
+        element.style.transition = elementTransition;
+        requestAnimationFrame(function() {
+            element.style.height = 'auto';
+        });
+  });*/
+    var sectionHeight = element.scrollHeight;
+
+    // have the element transition to the height of its inner content
+    element.style.height = sectionHeight + 'px';
+
+    // when the next css transition finishes (which should be the one we just triggered)
+    element.addEventListener('transitionend', function(e) {
+    // remove this event listener so it only gets triggered once
+    element.removeEventListener('transitionend', arguments.callee);
+
+    // remove "height" from the element's inline styles, so it can return to its initial value
+    element.style.height = null;
+    });
+}
+
+document.querySelector(".zoneClickCarte1").addEventListener('click', function(e){
+    listCarte.classList.toggle("open");
+    var isOpen = document.querySelector(".open") === listCarte;
+    if(isOpen){
+        heightOpen(listCarte);
+    }else{
+        heightClose(listCarte);
+    }
+});
